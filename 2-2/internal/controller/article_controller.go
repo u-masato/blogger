671a3df2384a032a3f5ab57de901b0937c49a2fd
u-masato <masato.u20@gmail.com>
2024-07-07 23:32:36 +0900

    📦 NEW: feat: Add create article endpoint to ArticleController with BindJson

--- 2-2/internal/controller/article_controller.go
+++ 2-2/internal/controller/article_controller.go
@@ -1,10 +1,22 @@
 package controller
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 type ArticleController struct{}
+
+type Article struct {
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+	Author  string `json:"author"`
+}
+
+var articlesMap = make(map[int]*Article)
+var articleNextID = 1
 
 func NewArticleController() *ArticleController {
 	return &ArticleController{}
@@ -12,7 +24,19 @@ func NewArticleController() *ArticleController {
 
 func (ctrl *ArticleController) GetAllArticles(c *gin.Context) {}
 
-func (ctrl *ArticleController) CreateArticle(c *gin.Context) {}
+func (ctrl *ArticleController) CreateArticle(c *gin.Context) {
+	var article Article
+	if err := c.ShouldBindJSON(&article); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	article.ID = articleNextID
+	articlesMap[article.ID] = &article
+	articleNextID++
+
+	c.JSON(http.StatusCreated, article)
+}
 
 func (ctrl *ArticleController) GetArticle(c *gin.Context) {}
 
