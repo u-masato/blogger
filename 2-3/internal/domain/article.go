d3a3091b1252b950532c274fd6abef30771c1c60
u-masato <masato.u20@gmail.com>
2024-07-08 01:48:15 +0900

    👌 IMPROVE: domain constructor 追加

--- 2-3/internal/domain/article.go
+++ 2-3/internal/domain/article.go
@@ -1,18 +1,84 @@
 package domain
 
-import "time"
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type ID uint
 
 type Title string
 
+const (
+	MaxTitleLength  = 255
+	MaxAuthorLength = 64
+)
+
+func NewTitle(title string) (Title, error) {
+	if title == "" {
+		return "", errors.New("title cannot be empty")
+	}
+	if len(title) > MaxTitleLength {
+		return "", fmt.Errorf("title length must be less than %d", MaxTitleLength)
+	}
+	return Title(title), nil
+}
+
 type Content string
+
+func NewContent(content string) (Content, error) {
+	if content == "" {
+		return "", errors.New("content cannot be empty")
+	}
+	return Content(content), nil
+}
 
 type Author string
 
+func NewAuthor(author string) (Author, error) {
+	if author == "" {
+		return "", errors.New("author cannot be empty")
+	}
+	if len(author) > MaxAuthorLength {
+		return "", fmt.Errorf("author length must be less than %d", MaxAuthorLength)
+	}
+	return Author(author), nil
+}
+
 type Article struct {
-	ID        uint
+	ID        ID
 	Title     Title
 	Content   Content
 	Author    Author
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+func NewArticle(id ID, title Title, content Content, author Author, createdAt, updatedAt time.Time) *Article {
+	return &Article{
+		ID:        id,
+		Title:     title,
+		Content:   content,
+		Author:    author,
+		CreatedAt: createdAt,
+		UpdatedAt: updatedAt,
+	}
+}
+
+func CreateArticle(id uint, title, content, author string) (*Article, error) {
+	i_d := ID(id)
+	t, err := NewTitle(title)
+	if err != nil {
+		return nil, err
+	}
+	c, err := NewContent(content)
+	if err != nil {
+		return nil, err
+	}
+	a, err := NewAuthor(author)
+	if err != nil {
+		return nil, err
+	}
+	return NewArticle(i_d, t, c, a, time.Now(), time.Now()), nil
 }
